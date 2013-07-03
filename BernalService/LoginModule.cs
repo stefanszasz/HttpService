63e43151b5679be988c6e735c33b911398d0a76b
Stefan Szasz <stefans@fortech.ro>
2013-07-03 16:43:35 +0300

    Initial commit

---
+++ BernalService/LoginModule.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using Nancy;
+
+namespace BernalService
+{
+    public class LoginModule : NancyModule
+    {
+        public LoginModule()
+        {
+            Get["/"] = Root;
+            Post["/Login"] = param =>
+                {
+                    var es = Login(param.userName, param.password);
+                    var response = (Response)es;
+                    response.ContentType = "application/json";
+                    return response;
+                };
+        }
+
+        public string Root(dynamic @params)
+        {
+            return "";
+        }
+
+        public string Login(string userName, string password)
+        {
+            using (var reader = new StreamReader(Request.Body))
+            {
+                string readToEnd = reader.ReadToEnd();
+                Console.WriteLine(readToEnd);
+                return
+                    "{ \"success\": true, \"user\": {\"Key\": 233, \"Value\": { \"Name\": \"someName\", \"Roles\": \"Role\", \"EMail\": \"email@email.com\", \"Phone\": \"122-334-44\", \"Address\": \"address\", \"Comment\": \"comment...\", \"WebAccess\": true } } }";
+            }
+        }
+    }
+}
---
+++ BernalService/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Nancy.Hosting.Self;
+
+namespace BernalService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nancyHost = new NancyHost(new Uri("http://localhost:990"));
+            nancyHost.Start();
+
+            Console.ReadKey();
+        }
+    }
+}
